c04184de8a90b8a8cc7c9a52370e1b216f8419a5
jackso-bytes <81561260+jackso-bytes@users.noreply.github.com>
2026-03-21 08:40:25 +0000

    Remove parserOptions from eslint config
    Removed parserOptions configuration from languageOptions.

--- eslint.config.js
+++ eslint.config.js
@@ -34,10 +34,6 @@ export default tseslint.config(
     },
     languageOptions: {
       globals: globals.browser,
-      parserOptions: {
-        project: ['./tsconfig.app.json', './tsconfig.test.json'],
-        tsconfigRootDir: import.meta.dirname,
-      },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
